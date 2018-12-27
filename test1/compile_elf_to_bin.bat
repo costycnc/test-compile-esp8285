8cf64b64360d7390cdcc5d34b378ccf196b5e908
Boboaca Costel <bobyca2003@yahoo.com>
2018-12-27 23:08:33 +0100

    Add files via upload

---
+++ test1/compile_elf_to_bin.bat
@@ -0,0 +1,4 @@
+C:\Users\costycnc\AppData\Roaming\Arduino15\packages\esp8266\tools\esptool\0.4.13/esptool.exe -bo test.cpp.bin -bm qio -bf 40 -bz 1M -bs .text -bp 4096 -ec -eo test.cpp.elf -bs .irom0.text -bs .text -bs .data -bs .rodata -bc -ec 
+
+
+Pause 0
---
+++ test1/esptool_help.bat
@@ -0,0 +1,6 @@
+echo off
+cls
+C:\Users\costycnc\AppData\Roaming\Arduino15\packages\esp8266\tools\esptool\0.4.13\esptool.exe
+-h
+
+Pause 0
